a0dbd72c85a2b92a8534498f2b40a0a900502f82
Rolu <roli4759@hotmail.com>
2015-03-25 19:22:54 +0100

    ProjectlistItemByRolu

--- app/src/main/java/hu/domain/ccc/commoncostscalculator/MainActivity.java
+++ app/src/main/java/hu/domain/ccc/commoncostscalculator/MainActivity.java
@@ -8,21 +8,67 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
 
 
 public class MainActivity extends ActionBarActivity {
     Button logoutBTN;
     String PrefFileName = "data";
+    List<Projects> projectItems = new ArrayList<>();
+    ListView projectList;
+    ProjectAdapter projectAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         MainActivity.this.setResult(1); //alapból visszára ne a belépésre dobjon
+
+        SharedPreferences settings = getSharedPreferences(PrefFileName, 0);
+        String session = settings.getString("session","");
+
+        HashMap<String, String> data = new HashMap<String, String>();
+        data.put("action", "get_projects");
+        data.put("session", session);
+        ServerConnect post = new ServerConnect(data);
+        try {
+            //JSON feldolgozása
+            JSONArray response = new JSONArray(post.execute("http://ccc.elitemagyaritasok.info").get());
+
+            for(int i=0;i<response.length();i++) {
+            JSONObject temp = response.getJSONObject(i);
+            DateFormat format = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
+            projectItems.add(new Projects(temp.getString("name"),format.parse(temp.getString("start_date")),temp.getString("description")));
+            }
+
+            projectList = (ListView)findViewById(R.id.Project_list);
+            projectAdapter = new ProjectAdapter(projectItems);
+            projectList.setAdapter(projectAdapter);
+
+            /*int ret = response.getInt("user_id");
+            if (ret>0) {
+                Toast.makeText(RegisterActivity.this, getString(R.string.registration_success), Toast.LENGTH_SHORT).show();
+                finish();
+            }
+            else if (ret==-1 || ret==-2){
+                Toast.makeText(RegisterActivity.this, getString(R.string.wrong_username_or_email), Toast.LENGTH_SHORT).show();
+            }*/
+
+        } catch (Exception e) {
+            Toast.makeText(MainActivity.this, e.toString(), Toast.LENGTH_LONG).show();
+        }
+
     }
 
 
